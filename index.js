1cc1577479698492680953b06d60dad4d6c2e92f
cucalondev <rapperanqeel@hotmail.com>
2023-12-27 15:04:42 +0100

    Añadiendo login

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-const {
-  app,
-  BrowserWindow,
-  globalShortcut,
-  session,
-  ipcMain
-} = require("electron");
-
+const { app, BrowserWindow, globalShortcut, session, ipcMain, Menu } = require("electron");
 const path = require("path");
-const launcherUrl = 'http://localhost:5439/';
 
-let mainWindow;
-let gameWindow = null;
-var iconpath = path.join(__dirname, "icon.ico");
-var pjson = require(__dirname + "/package.json");
+const launcherUrl = 'http://localhost:5439/';
+const iconpath = path.join(__dirname, "icon.ico");
+const pjson = require(__dirname + "/package.json");
 let pluginName;
 
 if (require("electron-squirrel-startup")) {
@@ -30,13 +21,11 @@ switch (process.platform) {
     pluginName = "flash/libpepflashplayer.so";
     break;
 }
-app.commandLine.appendSwitch(
-  "ppapi-flash-path",
-  path.join(__dirname, pluginName)
-);
+
+app.commandLine.appendSwitch("ppapi-flash-path", path.join(__dirname, pluginName));
 
 const createWindow = () => {
-  mainWindow = new BrowserWindow({
+  const mainWindow = new BrowserWindow({
     autoHideMenuBar: true,
     width: 1019,
     height: 687,
@@ -56,16 +45,29 @@ const createWindow = () => {
   mainWindow.setMenu(null);
   mainWindow.show();
 
-  mainWindow.webContents.on("did-finish-load", () => { });
+  mainWindow.webContents.on("did-finish-load", () => {});
 
-  mainWindow.on("closed", (event) => {
+  mainWindow.on("closed", () => {
     mainWindow = null;
   });
+
+  // Menú para abrir las DevTools
+  const menu = Menu.buildFromTemplate([
+    {
+      label: 'Developer',
+      submenu: [
+        { role: 'toggleDevTools' },
+      ],
+    },
+  ]);
+
+  Menu.setApplicationMenu(menu);
 };
 
 const createGameWindow = () => {
   if (gameWindow !== null) return;
-  gameWindow = new BrowserWindow({
+
+  const gameWindow = new BrowserWindow({
     autoHideMenuBar: true,
     width: 1219,
     height: 887,
@@ -80,43 +82,59 @@ const createGameWindow = () => {
     backgroundColor: pjson.backgroundColor,
     resizable: true,
   });
+
   gameWindow.loadURL(launcherUrl);
   gameWindow.setMenu(null);
   gameWindow.show();
 
   gameWindow.toggleDevTools();
 
-  gameWindow.webContents.on("did-finish-load", () => { });
-  gameWindow.on("closed", (event) => {
+  gameWindow.webContents.on("did-finish-load", () => {});
+  gameWindow.on("closed", () => {
     gameWindow = null;
   });
+
+  // Menú para abrir las DevTools
+  const menu = Menu.buildFromTemplate([
+    {
+      label: 'Developer',
+      submenu: [
+        { role: 'toggleDevTools' },
+      ],
+    },
+  ]);
+
+  Menu.setApplicationMenu(menu);
 };
 
 app.on("ready", () => {
   createWindow();
-  globalShortcut.register("f5", function () {
-    let win = BrowserWindow.getFocusedWindow();
+  globalShortcut.register("f5", () => {
+    const win = BrowserWindow.getFocusedWindow();
     if (win) {
       win.reload();
     }
   });
-  globalShortcut.register("f1", function () {
-    let win = BrowserWindow.getFocusedWindow();
+
+  globalShortcut.register("f1", () => {
+    const win = BrowserWindow.getFocusedWindow();
     if (win) {
       win.show();
     }
   });
-  globalShortcut.register("f3", function () {
-    let win = BrowserWindow.getFocusedWindow();
+
+  globalShortcut.register("f3", () => {
+    const win = BrowserWindow.getFocusedWindow();
     if (win) {
       win.toggleDevTools();
     }
   });
-  globalShortcut.register("f2", function () {
+
+  globalShortcut.register("f2", () => {
     session.defaultSession.clearCache();
   });
 });
 
-ipcMain.on('open-window', (event, arg) => {
+ipcMain.on('open-window', () => {
   createGameWindow();
 });
